d3680c6073e078fbfc3fddefa581da17e6bf46c6
smulch <mulcahy.samuel@gmail.com>
2023-03-06 22:50:49 -0500

    ran prettier on index

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const inquirer = require('inquirer');
-const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
-inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
+const inquirer = require("inquirer");
+const MaxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
+inquirer.registerPrompt("maxlength-input", MaxLengthInputPrompt);
 
 inquirer
   .prompt([
     {
-      type: 'maxlength-input',
-      message: 'Enter text for the logo',
-      name: 'text',
+      type: "maxlength-input",
+      message: "Enter text for the logo",
+      name: "text",
       maxLength: 3,
     },
     {
-      type: 'input',
-      message: 'Enter a color for for the text:',
-      name: 'color',
+      type: "input",
+      message: "Enter a color for for the text:",
+      name: "color",
     },
     {
-      type: 'input',
-      message: 'Enter a shape for the logo:',
-      name: 'shape',
+      type: "input",
+      message: "Enter a shape for the logo:",
+      name: "shape",
     },
     {
-        type: 'input',
-        message: 'Enter a color for the shape:',
-        name: 'background-color',
+      type: "input",
+      message: "Enter a color for the shape:",
+      name: "background-color",
     },
   ])
-  .then((response) =>
-    console.log(response)
-  );
+  .then((response) => console.log(response));
