8b6541f3bfbb2bb858fed43beb201cdcf0f327e9
smulch <mulcahy.samuel@gmail.com>
2023-03-06 20:47:53 -0500

    updated and fixed node modules

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-const inquirer = require('inquirer')
-const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
+const inquirer = require('inquirer');
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
+
+inquirer
+  .prompt([
+    {
+      type: 'maxlength-input',
+      message: 'Enter text for the logo',
+      name: 'text',
+      maxLength: 3,
+    },
+    {
+      type: 'input',
+      message: 'Enter a color for for the text:',
+      name: 'color',
+    },
+    {
+      type: 'input',
+      message: 'Enter a shape for the logo:',
+      name: 'shape',
+    },
+    {
+        type: 'input',
+        message: 'Enter a color for the shape:',
+        name: 'background-color',
+    },
+  ])
+  .then((response) =>
+    console.log(response)
+  );
