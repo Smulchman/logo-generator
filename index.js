1bda7760b96b8974677cd71d66bc751ea619cce2
smulch <mulcahy.samuel@gmail.com>
2023-03-07 20:03:14 -0500

    added comments

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+// Required modules
 const inquirer = require("inquirer");
+// Jake told be about this module, it allows me to limit the amount of characters that can be input and accepted. The input will just not be accepted until the user reduces the length.
 const MaxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
 inquirer.registerPrompt("maxlength-input", MaxLengthInputPrompt);
 const fs = require('fs');
 
+// Immporting all the shape classes
+// Not certain that importing the parent class is required, but I'm keeping it in
 const Shape = require("./lib/Shape");
 const Triangle = require("./lib/Triangle");
 const Circle = require("./lib/Circle");
 const Square = require("./lib/Square");
 
+// I'm declaring userShape here to give it global scope. It's representing shape object that the user is creating, later on in the code I create a shape based on the inputs and then store it in this variable.
 var userShape;
 
+// This array contains 3 objects that inquirer can read and use as choices during the prompts
 var shapesChoices = [
   {
     name: "Circle",
@@ -28,6 +34,7 @@ var shapesChoices = [
   },
 ];
 
+// Prompting the user for what they want their logo to look like.
 inquirer
   .prompt([
     {
@@ -38,7 +45,7 @@ inquirer
     },
     {
       type: "input",
-      message: "Enter a color for for the text (hexcode or appropriate keyword acceptable):",
+      message: "Enter a color for the text (hexcode or appropriate keyword will work):",
       name: "color",
     },
     {
@@ -49,12 +56,12 @@ inquirer
     },
     {
       type: "input",
-      message: "Enter a color for the shape (hexcode or appropriate keyword acceptable):",
+      message: "Enter a color for the shape (hexcode or appropriate keyword will work):",
       name: "backgroundColor",
     },
   ])
+//   Using a switch statement here to check for the shape the user chose and then building the right shape based on that. Basically a cleaner series of if statements.
   .then((response) => {
-    console.log(response);
     switch (response.shape) {
       case "Circle":
         userShape = new Circle(
@@ -78,11 +85,12 @@ inquirer
         );
         break;
     }
-    console.log(userShape);
-    console.log(userShape.render())
   })
+//   this is where the actual file is created. The first argument is just the name for the file and the second argument is the actual text that is going into the file as code.
   .then(() => {writeToFile("logo.svg", userShape.render())});
 
+//   This is where the function defining what writing the file actually looks like is defined. Uses fs.writeFile
+// Console logs an error if it doesnt work and console logs 'generated logo.svg' if it does work.
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => err ? console.log(err): console.log("Generated logo.svg"));
-}
+};
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,8 +1,11 @@
+// Importing all the dependencies
 const Shape = require("../lib/Shape");
 const Triangle = require("../lib/Triangle");
 const Circle = require("../lib/Circle");
 const Square = require("../lib/Square");
 
+// I've defined this variable up here to keep the formatting within the actual tests simple and consistent.
+// This variable is what the svg text should be in the example that is built below to be tested.
 var testCircle = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
 <circle cx="150" cy="100" r="80" fill="red" />
@@ -13,56 +16,69 @@ var testCircle = `<svg version="1.1" width="300" height="200" xmlns="http://www.
 
 
 describe("Shapes", () => {
-  // this tests to see if the object is a a shape
+  // This section is to test that objects are being created correctly according to their class.
   describe("instantiate", () => {
+    // Tests to see if our newShape is a Shape object
     it("should be an instance of the Shape class", () => {
       const newShape = new Shape();
       expect(newShape).toBeInstanceOf(Shape);
     });
+    // Tests to make sure the text value is assigned properly
     it('should have a text value of "shape"', () => {
       const newShape = new Shape("shape");
       expect(newShape.text).toBe("shape");
     });
+    // Tests to make sure the color value is assigned properly
     it('should have a color value of "red"', () => {
       const newShape = new Shape("shape", "red");
       expect(newShape.color).toBe("red");
     });
+    // Tests to make sure the render method throws an error by default
     it("should throw an error when render is called", () => {
       const newShape = new Shape();
       expect(newShape.render).toThrow();
     });
+    // Tests to see if our newTriangle is a Shape object
     it("should be an instance of the Shape class", () => {
       const newTriangle = new Triangle();
       expect(newTriangle).toBeInstanceOf(Shape);
     });
+    // Tests to see if our newTriangle is a Triangle object
     it("should be an instance of the Triangle class", () => {
       const newTriangle = new Triangle();
       expect(newTriangle).toBeInstanceOf(Triangle);
     });
+    // Tests to make sure the text value is assigned properly
     it('should have a text value of "triangle"', () => {
       const newTriangle = new Triangle("triangle");
       expect(newTriangle.text).toBe("triangle");
     });
+    // Tests to make sure the color value is assigned properly
     it('should have a color value of "orange"', () => {
       const newTriangle = new Triangle("triangle", "orange");
       expect(newTriangle.color).toBe("orange");
     });
+    // Tests to see if our newCircle is a Shape object
     it("should be an instance of the Shape class", () => {
       const newCircle = new Circle();
       expect(newCircle).toBeInstanceOf(Shape);
     });
+    // Tests to see if our newCircle is a Circle object
     it("should be an instance of the Circle class", () => {
       const newCircle = new Circle();
       expect(newCircle).toBeInstanceOf(Circle);
     });
+    // Tests to see if our newCircle render method is returning the expected value
     it("should have the proper SVG value", () => {
       const newCircle = new Circle("test", "white", "red");
       expect(newCircle.render()).toEqual(testCircle);
     });
+    // Tests to see if our newSquare is a Shape object
     it("should be an instance of the Shape class", () => {
       const newSquare = new Square();
       expect(newSquare).toBeInstanceOf(Shape);
     });
+    // Tests to see if our newSquare is a Square object
     it("should be an instance of the Square class", () => {
       const newSquare = new Square();
       expect(newSquare).toBeInstanceOf(Square);
