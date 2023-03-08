0a5a4cffc96ec6e69c4a49be055012ee59ffa12c
smulch <mulcahy.samuel@gmail.com>
2023-03-07 20:57:01 -0500

    added more tests and created a readme file

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -5,7 +5,7 @@ const Circle = require("../lib/Circle");
 const Square = require("../lib/Square");
 
 // I've defined these variables up here to keep the formatting within the actual tests simple and consistent.
-// The variables are what the svg text should be in the examples that built below in the tests.
+// The variables are what the svg text should be in the examples that are built below in the tests.
 var testCircle = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
 <circle cx="150" cy="100" r="80" fill="red" />
@@ -39,14 +39,19 @@ describe("Shapes", () => {
       expect(newShape).toBeInstanceOf(Shape);
     });
     // Tests to make sure the text value is assigned properly
-    it('should have a text value of "shape"', () => {
-      const newShape = new Shape("shape");
-      expect(newShape.text).toBe("shape");
+    it('should have a text value of "shp"', () => {
+      const newShape = new Shape("shp");
+      expect(newShape.text).toBe("shp");
     });
     // Tests to make sure the color value is assigned properly
     it('should have a color value of "red"', () => {
-      const newShape = new Shape("shape", "red");
+      const newShape = new Shape("shp", "red");
       expect(newShape.color).toBe("red");
+    });
+    // Tests to make sure the color value is assigned properly
+    it('should have a background value of "orange"', () => {
+      const newShape = new Shape("shp", "red", "orange");
+      expect(newShape.background).toBe("orange");
     });
     // Tests to make sure the render method throws an error by default
     it("should throw an error when render is called", () => {
@@ -64,14 +69,19 @@ describe("Shapes", () => {
       expect(newTriangle).toBeInstanceOf(Triangle);
     });
     // Tests to make sure the text value is assigned properly
-    it('should have a text value of "triangle"', () => {
-      const newTriangle = new Triangle("triangle");
-      expect(newTriangle.text).toBe("triangle");
+    it('should have a text value of "tri"', () => {
+      const newTriangle = new Triangle("tri");
+      expect(newTriangle.text).toBe("tri");
     });
     // Tests to make sure the color value is assigned properly
     it('should have a color value of "orange"', () => {
-      const newTriangle = new Triangle("triangle", "orange");
+      const newTriangle = new Triangle("tri", "orange");
       expect(newTriangle.color).toBe("orange");
+    });
+    // Tests to make sure the background value is assigned properly
+    it('should have a background value of "yellow"', () => {
+      const newTriangle = new Triangle("tri", "orange", "yellow");
+      expect(newTriangle.background).toBe("yellow");
     });
     // Tests to see if our newCircle is a Shape object
     it("should be an instance of the Shape class", () => {
@@ -83,6 +93,21 @@ describe("Shapes", () => {
       const newCircle = new Circle();
       expect(newCircle).toBeInstanceOf(Circle);
     });
+    // Tests to make sure the text value is assigned properly
+    it('should have a text value of "crc"', () => {
+      const newCircle = new Circle("crc");
+      expect(newCircle.text).toBe("crc");
+    });
+    // Tests to make sure the color value is assigned properly
+    it('should have a color value of "yellow"', () => {
+      const newCircle = new Circle("crc", "yellow");
+      expect(newCircle.color).toBe("yellow");
+    });
+    // Tests to make sure the color value is assigned properly
+    it('should have a background value of "blue"', () => {
+      const newCircle = new Circle("crc", "yellow", "blue");
+      expect(newCircle.background).toBe("blue");
+    });
     // Tests to see if our newSquare is a Shape object
     it("should be an instance of the Shape class", () => {
       const newSquare = new Square();
@@ -92,6 +117,21 @@ describe("Shapes", () => {
     it("should be an instance of the Square class", () => {
       const newSquare = new Square();
       expect(newSquare).toBeInstanceOf(Square);
+    });
+    // Tests to make sure the text value is assigned properly
+    it('should have a text value of "sqr"', () => {
+      const newSquare = new Square("sqr");
+      expect(newSquare.text).toBe("sqr");
+    });
+    // Tests to make sure the color value is assigned properly
+    it('should have a color value of "blue"', () => {
+      const newSquare = new Square("sqr", "blue");
+      expect(newSquare.color).toBe("blue");
+    });
+    // Tests to make sure the background value is assigned properly
+    it('should have a background value of "green"', () => {
+      const newSquare = new Square("sqr", "blue", "green");
+      expect(newSquare.background).toBe("green");
     });
   });
   describe("render", () => {
